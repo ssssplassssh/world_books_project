3c039d96a47f46160a069d3d5b6e8b24e4f8642e
ssssplassssh <igorrosolovskij2016@gmail.com>
2023-02-21 17:02:18 +0200

    15_commit_app

--- world_books/settings.py
+++ world_books/settings.py
@@ -150,8 +150,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Heroku: Обновление конфигурации базы данных из $DATAВASE_URL. 
 import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500) 
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
 
 
 # Отримуємо шлях до каталогу проекту
