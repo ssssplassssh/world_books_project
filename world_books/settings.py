74a03457582927009013639367f78485fb21f86b
ssssplassssh <igorrosolovskij2016@gmail.com>
2023-02-21 14:52:24 +0200

    11_commit_app

--- world_books/settings.py
+++ world_books/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'world_books.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
